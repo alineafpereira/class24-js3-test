a57295187b659e3bddc763f2e5340af11b991d55
alineafpereira <aline.amaralds@gmail.com>
2019-11-17 11:57:35 +0100

    made est

--- version2/test.js
+++ version2/test.js
@@ -17,6 +17,9 @@ const books = [
       "https://images-na.ssl-images-amazon.com/images/I/515iEcDr1GL._SX258_BO1,204,203,200_.jpg"
   }
 ];
+
+
+
 // - Iterates over the array and outputs the details of each item nto the DOM. 
 // - Displays the items inside an unordered list (created programmatically using JavaScript)
 
@@ -26,9 +29,48 @@ const books = [
 // - Uses the following URL: https://jsonplaceholder.typicode.com/users/1
 // - Displays the "name", "email" and "city" of the first person inside an unordered list (created programmatically using JavaScript) 
 
+  function createAndAppend(element, parent, options = {}){
+    let el = document.createElement(element);
+    parent.appendChild(el);
+    Object.entries(options).forEach(([key, value]) => {
+      if(key === 'text'){
+        el.textContent === value  
+      }else{
+        el.setAttribute(key, value)
+      }
+    })
+  }
+
+{
+  const url = 'https://jsonplaceholder.typicode.com/users/1';
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data){
+    data.map(function(user) {
+      console.log(user.name)
+      //const body = document.getElementsByTagName('body');
+      //createAndAppend('ul', body)
+    })
+  })
+}
+
+
 // 3.
 // Write a function that:
 // - Takes in an array of country names
 // - Returns the country with the longest name. 
 // - Uses the following array as an argument: 
-const countries = ["The Netherlands", "Syria", "Turkey", "Mexico", "United States"].
+
+
+const countries = ["The Netherlands", "Syria", "Turkey", "Mexico", "United States"];
+
+function checkLength(array){
+  const items = array.map(function(item){
+    console.log(item)
+    console.log(item.length)
+  })
+}
+
+console.log(checkLength(countries))
+
+  
